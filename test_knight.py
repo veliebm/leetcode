758e926183b6a47fd6da60f21425cb9c31a63609
Ben <veliebm@gmail.com>
2022-02-28 18:43:16 -0500

    Add new test cases

--- test_knight.py
+++ test_knight.py
@@ -1,4 +1,14 @@
 import knight
 
-def test_cases():
+
+def test_normal():
     assert knight.moves(4, 0, 0, 1, 1, 1, 0) == 4
+
+
+def test_unreachable():
+    assert knight.moves(5, 0, 0, 4, 3, 3, 0) == -1
+
+
+def test_trivial():
+    assert knight.moves(15, 12, 12, 12, 12, 10, 9) == 0
+    assert knight.moves(20, 12, 12, 12, 12, 11, 11) == 0
