e3946ec07dfe19759fc2421f247f449c165f74f1
Ben <veliebm@gmail.com>
2022-03-02 16:39:38 -0500

    Enable testing multiple cases

--- preorder /test_preorder.py
+++ preorder /test_preorder.py
@@ -1,15 +1,22 @@
 import sys
 from unittest.mock import Mock
 
-def test_prompt(capsys, monkeypatch):
-    t = "6"
-    arr = "1 2 5 3 6 4"
-    stdout = "1 2 5 3 4 6 "
+
+def test_cases(capsys, monkeypatch):
+    cases = [
+        ("6", "1 2 5 3 6 4", "1 2 5 3 4 6 ")
+    ]
+
+    for case in cases:
+        run_test(*case, capsys, monkeypatch)
+
+
+def run_test(t, arr, stdout, capsys, monkeypatch):
     yield_input = Mock(side_effect=[t, arr])
 
     with monkeypatch.context() as m:
         m.setattr("builtins.input", yield_input)
         import preorder
-        del sys.modules["preorder"]
         captured = capsys.readouterr()
         assert captured.out == stdout
+        del sys.modules["preorder"]
