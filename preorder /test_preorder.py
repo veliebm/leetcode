7600873e40d02e7bcfb492ca4c2d17a7b691dd2e
Ben <veliebm@gmail.com>
2022-03-02 16:33:25 -0500

    Greatly simplify mock function

--- preorder /test_preorder.py
+++ preorder /test_preorder.py
@@ -1,16 +1,11 @@
 import sys
+from unittest.mock import Mock
 
 def test_prompt(capsys, monkeypatch):
     t = "6"
     arr = "1 2 5 3 6 4"
     stdout = "1 2 5 3 4 6 "
-
-    def yield_input():
-        def get():
-            temp = yield_input.__dict__.get('previous', t)
-            yield_input.__dict__['previous'] = arr
-            return temp
-        return get()
+    yield_input = Mock(side_effect=[t, arr])
 
     with monkeypatch.context() as m:
         m.setattr("builtins.input", yield_input)
