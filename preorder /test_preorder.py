e781b6d767fa6b52589fd8c976aad6ac8c1bb52d
Ben <veliebm@gmail.com>
2022-03-02 16:55:52 -0500

    Rename vars, add docstrings

--- preorder /test_preorder.py
+++ preorder /test_preorder.py
@@ -3,16 +3,18 @@ from unittest.mock import Mock
 
 
 def test_cases(capsys, monkeypatch):
+    """Run all test cases."""
     cases = [
-        ("6", "1 2 5 3 6 4", "1 2 5 3 4 6 ")
+        ("6\n1 2 5 3 6 4", "1 2 5 3 4 6 ")
     ]
 
     for case in cases:
         run_test(*case, capsys, monkeypatch)
 
 
-def run_test(t, arr, stdout, capsys, monkeypatch):
-    yield_input = Mock(side_effect=[t, arr])
+def run_test(stdin, stdout, capsys, monkeypatch):
+    """Test a case."""
+    yield_input = Mock(side_effect=stdin.splitlines())
 
     with monkeypatch.context() as m:
         m.setattr("builtins.input", yield_input)
