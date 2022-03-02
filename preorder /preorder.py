2fab4ba169d2fd90688d8832b56d4e57a3f060ea
Ben <veliebm@gmail.com>
2022-03-02 14:27:40 -0500

    Make test framework

---
+++ preorder /preorder.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+# --------- BEGIN SOLUTION --------------
+"""
+https://www.hackerrank.com/challenges/one-week-preparation-kit-tree-preorder-traversal/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-seven
+
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    pass
+# --------- END SOLUTION --------------
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
---
+++ preorder /test_preorder.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+def test_normal():
+    """Test a regular case."""
+    generate_test(input=b'6\n1 2 5 3 6 4\n\n', output=b'1 \n2 \n5 \n3 \n6 \n4 \n')
+
+
+def generate_test(input, output):
+    result = subprocess.run("python ./preorder.py".split(), input=input, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    assert result.returncode == 0
+    assert result.stdout == output
+    assert result.stderr == b''
