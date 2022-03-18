2b05108db849479f5fdb55c9cafc3f52cac29d5a
Ben <veliebm@gmail.com>
2022-03-17 21:47:18 -0400

    make work

--- leet.js
+++ leet.js
@@ -1,8 +1,8 @@
 var climbStairs = function (n) {
   let [a, b] = [1, 1];
-  for (_ = 0; _ < n; _++) {
-    [a, b] = [a, a + b];
+  for (let _ = 0; _ < n; _++) {
+    [a, b] = [b, a + b];
   }
   return a;
 };
-console.log(1)
+console.log(1);
